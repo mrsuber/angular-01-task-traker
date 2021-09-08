b8755718cd891bbf850ee1954fbb877c80860a36
mohamad <msiysinyuy@gmail.com>
2021-09-08 13:40:44 +0100

    deploying on heroku

---
+++ task-tracker-frontend/src/firebase.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAa1sBRpeacX14mm-5Sr4N0TqCRsW0sxkI",
+  authDomain: "angular-app-v1.firebaseapp.com",
+  projectId: "angular-app-v1",
+  storageBucket: "angular-app-v1.appspot.com",
+  messagingSenderId: "907317378162",
+  appId: "1:907317378162:web:d20a4495e79dff66b2d6eb",
+  measurementId: "G-3YF8SZ025K"
+};
