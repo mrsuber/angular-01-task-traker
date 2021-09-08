db4de7674eca04409b4da5dbb8af5c2cd5f17145
mohamad <msiysinyuy@gmail.com>
2021-09-08 09:49:13 +0100

    task traker can and updat task, add task, delete task all using a json database

---
+++ task-tracker-frontend/src/app/mock-tasks.ts
@@ -0,0 +1,33 @@
+import {Task} from './Task'
+export const TASKS : Task[]=[
+  {
+    id:1,
+    text:'Doctors Appointment',
+    day:'May 5th at 2:20',
+    reminder:true,
+  },
+  {
+    id:2,
+    text:'Meet at School',
+    day:'May 15th at 2:30',
+    reminder:true,
+  },
+  {
+    id:3,
+    text:'Food Shopping',
+    day:'May 6th at 2:20',
+    reminder:true,
+  },
+  {
+    id:4,
+    text:'Doctors Appointment',
+    day:'May 5th at 2:50',
+    reminder:true,
+  },
+  {
+    id:5,
+    text:'Doctors Appointment',
+    day:'May 5th at 2:20',
+    reminder:false,
+  }
+]
